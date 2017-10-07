b641de07e141e322b95fdfbf88091fab85508193
WRMcFadden <w_r_mcfadden@hotmail.com>
2017-10-07 13:06:16 -0400

    Create AppBasics Helper Class

---
+++ src/main/java/com/sqa/wrm/helpers/exceptions/RequestIntOverMaxException.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: RequestIntOverMaxException.java<br>
+ *
+ *   McFadden, Wayne<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Oct 7, 2017
+ *   
+ */
+
+package com.sqa.wrm.helpers.exceptions;
+
+
+/**
+ * RequestIntOverMaxException //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author      McFadden, Wayne
+ * @version     1.0.0
+ * @since       1.0
+ *
+ */
+public class RequestIntOverMaxException extends Exception {
+}
